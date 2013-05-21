2f897366f4dd9259401558a040b78bae883f247e
Manuel Zambrano <mmzmm3z@gmail.com>
2013-05-21 19:04:00 -0430

    modificando para el logeo de usuarios

--- src/web/protected/components/UserIdentity.php
+++ src/web/protected/components/UserIdentity.php
@@ -17,11 +17,13 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
+		$model=new Users;
+		/*$users=array(
 			// username => password
 			'demo'=>'demo',
 			'admin'=>'admin',
-		);
+		);*/
+		$model->find('username=:usuario', array(':usuario'=>$this->username));
 		if(!isset($users[$this->username]))
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
 		elseif($users[$this->username]!==$this->password)
