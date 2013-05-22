a190544de77c3b15c3e0a9a31c5077edcebb4afa
Manuel Zambrano <mmzmm3z@gmail.com>
2013-05-22 16:20:35 -0430

    agregadas las vistas de las acciones compras y ventas

--- src/web/protected/views/balance/compras.php
+++ src/web/protected/views/balance/compras.php
@@ -23,6 +23,41 @@ $this->widget('zii.widgets.grid.CGridView',array(
 		array(
 			'name'=>'asr',
 			),
+		array(
+			'name'=>'margin_percentage',
+			),
+		array(
+			'name'=>'margin_per_minute',
+			),
+		array(
+			'name'=>'cost_per_minute',
+			),
+		array(
+			'name'=>'revenue_per_min',
+			),
+		array(
+			'name'=>'pdd',
+			),
+		array(
+			'name'=>'incomplete_calls',
+			),
+		array(
+			'name'=>'complete_calls_ner',
+			),
+		array(
+			'name'=>'complete_calls',
+			),
+		array(
+			'name'=>'calls_attempts',
+			),
+		array(
+			'name'=>'duration_real',
+			),
+		array(
+			'header'=>'Detalle',
+			'class'=>'CButtonColumn',
+			'template'=>'{view}',
+			),
 		),
 	));
 ?>
