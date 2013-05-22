8a24c676e3867beef2cc6c2fa764a9844713ea3a
Manuel Zambrano <mmzmm3z@gmail.com>
2013-05-22 16:21:12 -0430

    modificacion de links en el layout

--- src/web/protected/views/layouts/main.php
+++ src/web/protected/views/layouts/main.php
@@ -30,7 +30,7 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'Carrier', 'url'=>array('/carrier/index'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Carrier', 'url'=>array('/carrier'), 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Ventas', 'url'=>array('/balance/ventas'), 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Compras', 'url'=>array('/balance/compras'), 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Reportes', 'url'=>array('/balance/reportes'), 'visible'=>!Yii::app()->user->isGuest),
