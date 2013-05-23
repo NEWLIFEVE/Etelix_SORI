9775f869dd1ed4c44b192017438f029fc6d97487
edmaku <eduardom@etelix@gmail.com>
2013-05-23 12:51:11 -0430

    copmentado el menu

--- src/web/protected/views/balance/view.php
+++ src/web/protected/views/balance/view.php
@@ -7,16 +7,16 @@ $this->breadcrumbs=array(
 	$model->id,
 );
 
-$this->menu=array(
-	array('label'=>'List Balance', 'url'=>array('index')),
-	array('label'=>'Create Balance', 'url'=>array('create')),
-	array('label'=>'Update Balance', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Balance', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Balance', 'url'=>array('admin')),
-);
+//$this->menu=array(
+//	array('label'=>'List Balance', 'url'=>array('index')),
+//	array('label'=>'Create Balance', 'url'=>array('create')),
+//	array('label'=>'Update Balance', 'url'=>array('update', 'id'=>$model->id)),
+//	array('label'=>'Delete Balance', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+//	array('label'=>'Manage Balance', 'url'=>array('admin')),
+//);
 ?>
 
-<h1>View Balance #<?php echo $model->id; ?></h1>
+<h1>Balance #<?php echo $model->id; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
