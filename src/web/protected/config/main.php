2f5816d759f18c3bb07a16be5dc7e74e3f08587b
Manuel Zambrano <mmzmm3z@gmail.com>
2013-06-18 17:32:38 -0430

    eliminando rastro de vieja extension

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -53,7 +53,7 @@ return array(
 		),*/
 		// uncomment the following to use a MySQL database
 		'db'=>array(
-			'connectionString' => 'pgsql:host=localhost;port=5432;dbname=sori',
+			'connectionString' => 'pgsql:host=192.168.1.140;port=5432;dbname=sori',
 			'emulatePrepare' => true,
 			'username' => 'postgres',
 			'password' => '123',
