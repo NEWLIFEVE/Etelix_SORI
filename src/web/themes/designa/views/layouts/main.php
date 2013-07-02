8006e6585aeb08888e99f0a38c5d94db2ff95040
Manuel Zambrano <mmzmm3z@gmail.com>
2013-07-02 17:39:53 -0430

    se elimino capas ocultas del main, se crean ahora por javascript, se acomodo el estilo da las capas en divs.css

--- src/web/themes/designa/views/layouts/main.php
+++ src/web/themes/designa/views/layouts/main.php
@@ -119,13 +119,6 @@
 			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 			s.parentNode.insertBefore(g,s)}(document,'script'));
 	</script>
-	<div class="transparente oculta"></div>
-	<div class="loading oculta">
-		<h1>CARGANDO ARCHIVOS</h1>
-		<p>Este proceso puede tardar unos minutos</p>
-		<p>Por favor espere</p>
-		<img src="<?php echo Yii::app()->baseUrl; ?>/images/image_464753.gif">
-	</div>
 </body>
 </html>
 <?php
