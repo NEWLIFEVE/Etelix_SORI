aca6013c87ce57120f743eaeaccfcff5ab93e427
Manuel Zambrano <mmzmm3z@gmail.com>
2013-07-02 15:15:59 -0430

    no deja enviar el formulario en la subida de archivo hasta que seleccione una opcion

--- src/web/js/views.js
+++ src/web/js/views.js
@@ -5,24 +5,29 @@ $(document).on('ready',function()
 		$("div.diario").fadeIn("slow").css({'display':'block'});
 		$("div.horas").fadeOut("slow");
 		$("div.rerate").fadeOut("slow");
-		console.log("Funciona dia");
 	});
 	$('input[value="hora"]').on('click',function()
 	{
 		$("div.horas").fadeIn("slow").css({'display':'block'});
 		$("div.diario").fadeOut("slow");
 		$("div.rerate").fadeOut("slow");
-		console.log("Funciona hora");
 	});
 	$('input[value="rerate"]').on('click',function()
 	{
 		$("div.rerate").fadeIn("slow").css({'display':'block'});
 		$("div.diario").fadeOut("slow");
 		$("div.horas").fadeOut('slow');
-		console.log("Funciona rerate");
 	});
-	$('form[name="monto"]').on('submit',function()
-	{
-		$("div.transparente, div.loading").fadeIn('slow').removeClass('oculta');
+	$('form[name="monto"]').on('submit',function(e)
+	{		
+		if($("input:checked").val()==undefined)
+		{
+			e.preventDefault();
+			alert("Debe seleccionar una opcion");
+		}
+		else
+		{
+			$("div.transparente, div.loading").fadeIn('slow').removeClass('oculta');
+		}
 	});
 });
