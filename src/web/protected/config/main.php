5353071b6917f27dbad1b7bb8355882f944cbbcc
Manuel Zambrano <mmzmm3z@gmail.com>
2013-07-18 14:41:11 -0430

    commit para realizar merge a master

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -54,11 +54,11 @@ return array(
 		// uncomment the following to use a MySQL database
 		'db'=>array(
 
-			'connectionString' => 'pgsql:host=192.168.1.239;port=5432;dbname=sori',
+			'connectionString' => 'pgsql:host=localhost;port=5432;dbname=sori',
 
 			'emulatePrepare' => true,
 			'username' => 'postgres',
-			'password' => '123',
+			'password' => 'Nsusfd8263',
 			'charset' => 'utf8',
 		),
 		'errorHandler'=>array(
