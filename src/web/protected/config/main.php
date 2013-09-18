1eb07b5b80337b67a01f1f085660257994fcbcd6
NEWLIFEVE <mark182182@gmail.com>
2013-09-18 11:46:29 -0430

    commit por merge

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -57,12 +57,12 @@ return array(
 
             'db'=>array(
 
-                        'connectionString'=>'pgsql:host=192.168.1.29;port=5432;dbname=sori',
-                        //'connectionString'=>'pgsql:host=localhost;port=5432;dbname=sori',
+                        //'connectionString'=>'pgsql:host=192.168.1.29;port=5432;dbname=sori',
+                        'connectionString'=>'pgsql:host=localhost;port=5432;dbname=sori',
                                     'emulatePrepare'=>true,
                                     'username'=>'postgres',
-                                    'password'=>'123',
-                                    //'password'=>'Nsusfd8263',
+                                    //'password'=>'123',
+                                    'password'=>'Nsusfd8263',
                                     'charset'=>'utf8',
                         ),
 
