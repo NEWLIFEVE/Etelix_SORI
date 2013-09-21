351c86173ffba70a02c322091cc9c9fd4507287d
Manuel Zambrano <mmzmm3z@gmail.com>
2013-09-21 12:18:46 -0400

    arreglando tonterias

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -58,7 +58,7 @@ return array(
             'db'=>array(
 
                         //'connectionString'=>'pgsql:host=192.168.1.29;port=5432;dbname=sori',
-                        'connectionString'=>'pgsql:host=localhost;port=5432;dbname=dev_sori',
+                        'connectionString'=>'pgsql:host=localhost;port=5432;dbname=sori',
                                     'emulatePrepare'=>true,
                                     'username'=>'postgres',
                                     //'password'=>'123',
