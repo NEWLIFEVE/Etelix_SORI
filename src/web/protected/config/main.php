2d27d67392439567fc811563a9a833f019bb4445
Manuel Zambrano <mmzmm3z@gmail.com>
2013-09-21 11:48:00 -0430

    cambio de generacion de modelo de estatico a objeto

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -49,7 +49,7 @@ return array(
 			'connectionString'=>'pgsql:host=localhost;port=5432;dbname=sori',
 			'emulatePrepare'=>true,
 			'username'=>'postgres',
-			'password'=>'123',
+			'password'=>'Nsusfd8263',
 			'charset'=>'utf8',
 			),
 		'errorHandler'=>array(
