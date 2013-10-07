a23e06765032bf744823dfeb6ea96ce64b1c3daa
NEWLIFEVE <mark182182@gmail.com>
2013-10-07 16:42:36 -0430

    a produccion

--- src/web/protected/config/main.php
+++ src/web/protected/config/main.php
@@ -42,7 +42,7 @@ return array(
                 ),
             ),
         'db'=>array(
-            'connectionString'=>'pgsql:host=localhost;port=5432;dbname=dev_sori',
+            'connectionString'=>'pgsql:host=localhost;port=5432;dbname=sori',
             'emulatePrepare'=>true,
             'username'=>'postgres',
             'password'=>'Nsusfd8263',
