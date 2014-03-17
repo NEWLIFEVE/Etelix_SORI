857e4a223fdf0a65cdd5b246d60dc48c89133032
Manuel Zambrano <mmzmm3z@gmail.com>
2014-03-17 15:23:48 -0430

    estableciendo entornos de desarrollo

--- src/web/index.php
+++ src/web/index.php
@@ -1,13 +1,32 @@
 <?php
 date_default_timezone_set('America/Caracas');
-// change the following paths if necessary
+//Definimos nuestro servidor de produccion
+define('SERVER_NAME_PROD','sori.sacet.com.ve');
+//Definimos nuestro servidor de preproduccion
+define('SERVER_NAME_PRE_PROD','devs.sacet.com.ve');
+//Definimos nuestro servidor de desarrollo
+define('SERVER_NAME_DEV','sori.local');
+//Obtenemos el nombre del servidor actual
+$server=$_SERVER['SERVER_NAME'];
+
 $yii='../../../yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
 
-// remove the following lines when in production mode
-//defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
+switch ($server)
+{
+	case SERVER_NAME_PROD:
+		defined('YII_DEBUG') or define('YII_DEBUG',false);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',0);
+		break;
+	case SERVER_NAME_PRE_PROD:
+		defined('YII_DEBUG') or define('YII_DEBUG',true);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+		break;
+	case SERVER_NAME_DEV:
+	default:
+		defined('YII_DEBUG') or define('YII_DEBUG',true);
+		defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+		break;
+}
 require_once($yii);
 Yii::createWebApplication($config)->run();
