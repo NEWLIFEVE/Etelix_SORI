f5a68586a3248b1de1978fc147a7aa682b4245eb
abelardo <abelardotorres87@gmail.com>
2014-05-20 17:05:59 -0430

    cambio de nombre al archivo

--- src/web/protected/components/ValidationsArchCapt.php
+++ src/web/protected/components/ValidationsArchCapt.php
@@ -6,7 +6,7 @@
  * @copyright 2013 Sacet Todos los derechos reservados.
  * @package components
  */
-class ValidacionesArchCapt
+class ValidationsArchCapt
 {
     public $model;
     public $vencom;
@@ -17,15 +17,12 @@ class ValidacionesArchCapt
     public $log;
     public $fecha;
     public $nombreArchivo;
-    
-    public $valida;
-
     private $nuevos=0;
     private $actualizados=0;
     private $fallas=0;
     private $destino;
-    
     public $excel;
+    public $valida;
     
     //errores de log
     const ERROR_SAVE_LOG=6;
@@ -41,26 +38,17 @@ class ValidacionesArchCapt
     const ERROR_ESTRUC=1;
     //No hay errores
     const ERROR_NONE=0;
+    
+   
 
     /**
      *
      */
     
-    public function validar($path,$diario) {
-    	
-    	
-    	$date=date('Y-m-d');
-		$yesterday=strtotime('-1 day',strtotime($date));
-		$yesterday=date('Y-m-d',$yesterday);
-    	
-   		//Nombres opcionales para los archivos diarios
-		$diarios=array(
-			'Carga Ruta Internal'=>'Ruta Internal Diario',
-			'Carga Ruta External'=>'Ruta External Diario'
-			);
-
-		//Primero: verifico que archivos están
-		$existentes=$this->getNombreArchivos($path,$diarios,array('xls','XLS'));
+    public function validar($path,$diario,$existentes,$yesterday) 
+    {
+    	//Primero: verifico que archivos están
+        //$existentes=$this->getNombreArchivos($path,$diarios,array('xls','XLS'));
 		
 		if(count($existentes)<=0)
 		{
@@ -93,90 +81,14 @@ class ValidacionesArchCapt
 			//Cuarto: valido el orden de las columnas
 			$this->validarColumnas($this->lista($diario));
 		}
-    }
-    
-    
-    protected  function setName($nombre)
-    {
-        $this->nombreArchivo=$nombre;
-    }
-    
-    /**
-     * Agrega al objeto del reader el archivo excel que se va a grabar
-     * @param $ruta string ubicacion del archivo
-     */
-    public function carga($ruta)
-    {
-        //importo la extension
-        Yii::import("ext.Excel.Spreadsheet_Excel_Reader");
-        //oculto errores
-        error_reporting(E_ALL ^ E_NOTICE);
-
-        $this->excel = new Spreadsheet_Excel_Reader();
-        //uso esta codificacion ya que dio problemas usando utf-8 directamente
-        $this->excel->setOutputEncoding('ISO-8859-1');
-        $this->excel->read($ruta);
-    }
-    
-    /**
-    * Esta funcion se encarga de definir que nombre darle al archivo al momento de guardarlo en el servidor
-    */
-    public static function nombre($nombre)
-    {
-        $primero="Ruta ";
-        $segundo="External ";
-        $tercero="Diario";
-        if(stripos($nombre,"internal"))
-        {
-            $segundo="Internal ";
-        }
-        if(stripos($nombre,'rerate') || stripos($nombre, "RR"))
-        {
-            $tercero="RR";
-        }
-        if(stripos($nombre,'GMT'))
-        {
-            $tercero="Hora";
-        }
-        $nuevoNombre=$primero.$segundo.$tercero;
-        return $nuevoNombre;     
-    }
-    /**
-    * Encargada de definir atributos para proceder a la lectura del archivo
-    */
-    public function define($nombre)
-    {
-        if(stripos($nombre,"internal"))
-        {
-            $this->tipo="internal";
-            $this->destino="id_destination_int";
-        }
-        else
-        {
-            $this->tipo="external";
-            $this->destino="id_destination";
-        }
-    }
-    /**
-    * Funcion a la que se le pasa una lista donde el orden incluido debe ser cumplido por el archivo que se esta evaluando
-    * @param array $lista lista de elementos que debe cumplir las columnas
-    */
-    public function validarColumnas($lista)
-    {
-        foreach ($lista as $key => $campo)
-        {
-            $pos=$key+1;
-            if($campo!=$this->excel->sheets[0]['cells'][2][$pos])
-            {
-                $this->error=self::ERROR_ESTRUC;
-                $this->errorComment.="<h5 class='nocargados'> El archivo '".$this->nombreArchivo."' tiene la columna ".$this->excel->sheets[0]['cells'][2][$pos]." en lugar de ".$campo."</h5> <br/>";
-                return false;
-            }
-        }
-        $this->error=self::ERROR_NONE;
-        return true;
-    }
-    /**
+		
+		if($this->error==0){
+			return true;
+		}else{
+			return false;
+		}
+	 }
+ /**
     * Encargado de traer los nombres de los archivos que coinciden con la lista dada
     * @param $directorio string ruta al directorio que se va a revisar
     * @param $listaArchivos array lista de archivos que se van a buscar en el directorio
@@ -210,7 +122,28 @@ class ValidacionesArchCapt
             return $confirmados;
         }
     }
+    
+    protected  function setName($nombre)
+    {
+        $this->nombreArchivo=$nombre;
+    }
     /**
+    * Encargada de definir atributos para proceder a la lectura del archivo
+    */
+    public function define($nombre)
+    {
+        if(stripos($nombre,"internal"))
+        {
+            $this->tipo="internal";
+            $this->destino="id_destination_int";
+        }
+        else
+        {
+            $this->tipo="external";
+            $this->destino="id_destination";
+        }
+    }
+	/**
      * Valida que el archivo que se esta leyendo no este en log,
      * si existe deveulve verdadero de lo contrario falso y asigna el valor del log
      * @param $key string con el nombre del archivo que se quiere verificar
@@ -248,7 +181,23 @@ class ValidacionesArchCapt
             return false;
         }
     }
-    /**
+     /**
+     * Agrega al objeto del reader el archivo excel que se va a grabar
+     * @param $ruta string ubicacion del archivo
+     */
+    public function carga($ruta)
+    {
+        //importo la extension
+        Yii::import("ext.Excel.Spreadsheet_Excel_Reader");
+        //oculto errores
+        error_reporting(E_ALL ^ E_NOTICE);
+
+        $this->excel = new Spreadsheet_Excel_Reader();
+        //uso esta codificacion ya que dio problemas usando utf-8 directamente
+        $this->excel->setOutputEncoding('ISO-8859-1');
+        $this->excel->read($ruta);
+    }
+ /**
     *
     */
     public function validarFecha($fecha)
@@ -267,6 +216,49 @@ class ValidacionesArchCapt
             $this->errorComment="<h5 class='nocargados'> El archivo '".$this->nombreArchivo."' tiene una fecha incorrecta </h5> <br/> ";
             return false;
         }
+    }
+ /**
+    * Funcion a la que se le pasa una lista donde el orden incluido debe ser cumplido por el archivo que se esta evaluando
+    * @param array $lista lista de elementos que debe cumplir las columnas
+    */
+    public function validarColumnas($lista)
+    {
+        foreach ($lista as $key => $campo)
+        {
+            $pos=$key+1;
+            if($campo!=$this->excel->sheets[0]['cells'][2][$pos])
+            {
+                $this->error=self::ERROR_ESTRUC;
+                $this->errorComment.="<h5 class='nocargados'> El archivo '".$this->nombreArchivo."' tiene la columna ".$this->excel->sheets[0]['cells'][2][$pos]." en lugar de ".$campo."</h5> <br/>";
+                return false;
+            }
+        }
+        $this->error=self::ERROR_NONE;
+        return true;
+    }
+    
+    /**
+    * Esta funcion se encarga de definir que nombre darle al archivo al momento de guardarlo en el servidor
+    */
+    public static function nombre($nombre)
+    {
+        $primero="Ruta ";
+        $segundo="External ";
+        $tercero="Diario";
+        if(stripos($nombre,"internal"))
+        {
+            $segundo="Internal ";
+        }
+        if(stripos($nombre,'rerate') || stripos($nombre, "RR"))
+        {
+            $tercero="RR";
+        }
+        if(stripos($nombre,'GMT'))
+        {
+            $tercero="Hora";
+        }
+        $nuevoNombre=$primero.$segundo.$tercero;
+        return $nuevoNombre;     
     }
     /**
 	* Retorna un arreglo con los nombres de las columnas que deberian tener los archivos
@@ -297,6 +289,57 @@ class ValidacionesArchCapt
         	);
         return $lista[$nombre];
 	}
+	
+	//   funcion que crea un string con los datos preliminar
+	public static function load_arch_temp($fecha,$id_destination,$id_destination_int)
+    {
+       $connection=Yii::app()->db;
+       //verifico si hay algun registro prelminar del dia guardado 
+       //falta identificar si es inter o exter para no borrar esos registros
+       $sql1="select count(id) as count_records from balance where date_balance='".$fecha."'";
+       $command=$connection->createCommand($sql1);
+	   $res=$command->queryRow();					
+	   $count_records=0;
+	   if($res){
+		   $count_records=$res['count_records'];
+	   }		
+		
+	   if($count_records>0)
+	   {
+	   	   if($id_destination==NULL){ // ES interno de acuerdo a lo que voy a guardar
+	    	  $sql="select id,id_destination,id_destination_int from balance where id_destination is NULL and date_balance='".$fecha."'";
+		   }elseif($id_destination_int==NULL){ //es externo de acuerdo a lo que voy a guardar
+    	     $sql="select id,id_destination,id_destination_int from balance where id_destination_int is NULL anddate_balance='".$fecha."'";
+		   }
+	   	   //string para guardar los id (datos preliminar) antes de borrarlos
+	     
+	       $command=$connection->createCommand($sql);
+	       $results=$command->queryAll();
+	       $i=0; 
+	       foreach($results as $x=>$row) 
+			{
+			  $values.=" id=".$row['id']." ";
+			  if($i<$count_records-1)
+			  {
+			   $values.=" or";
+			  }
+			  $i++;
+			 }
+		 
+		}
+        return  $values;
+	}
+	
+	//funcion que borra el string generado con los datos preliminar
+    public static function delete_arch_temp($string_data_preliminar)
+    {
+    	// busco si estan cargados los internos o externos  para borrarlo
+        $sql="DELETE FROM balance where ".$cadena." ";
+		$command = Yii::app()->db->createCommand($sql);
+		if($command->execute()){
+		   $this->error=self::ERROR_NONE;
+		}
+    }
     
 }
 ?>
