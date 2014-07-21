45b6b2ba226f5ce9ee713b05bbd9d4efdfdd70a2
Manuel Zambrano <mmzmm3z@gmail.com>
2014-07-21 15:57:54 -0430

    ajuste en la funcion que ordena los registros a guardar

--- src/web/protected/components/Reader.php
+++ src/web/protected/components/Reader.php
@@ -87,9 +87,7 @@ class Reader
                             {
                                 //Obtengo el id de destino externo
                                 $valores['id_destination']=Destination::getId(utf8_encode($archivo->excel->sheets[0]['cells'][$i][$j]));
-                                $valores['id_destination_int']='NULL';
-                                $id_destination_int=$valores['id_destination_int'];
-                                $id_destination=$valores['id_destination'];                                
+                                $valores['id_destination_int']='NULL';                              
                             }
                             else
                             {
@@ -97,6 +95,8 @@ class Reader
                                 $valores['id_destination_int']=DestinationInt::getId(utf8_encode($archivo->excel->sheets[0]['cells'][$i][$j]));
                                 $valores['id_destination']='NULL';
                             }
+                            $id_destination_int=$valores['id_destination_int'];
+                            $id_destination=$valores['id_destination'];  
                         }
                         break;
                     case 2:
--- src/web/protected/config/console.php
+++ src/web/protected/config/console.php
@@ -4,12 +4,7 @@
 // Any writable CConsoleApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-<<<<<<< HEAD
 	'name'=>'Consola SORI',
-=======
-	'name'=>'SORI Consola',
-	'timeZone'=>'America/Caracas',
->>>>>>> cargaValidaciones
 	// preloading 'log' component
 	'preload'=>array('log'),
 	'import'=>array(
